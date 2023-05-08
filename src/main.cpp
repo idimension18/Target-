394db743cc27f47a67ea0b6a16902fb9abe3df12
idimension <idimension_derouet@hotmail.com>
2023-05-08 14:30:33 +0200

    test on linux

--- src/main.cpp
+++ src/main.cpp
@@ -10,10 +10,10 @@
 # include <string>
 # include <sstream>
 
-# include "SDL.h"
-# include "SDL_mixer.h"
-# include "SDL_image.h"
-# include "SDL_ttf.h"
+# include <SDL2/SDL.h>
+# include <SDL2/SDL_mixer.h>
+# include <SDL2/SDL_image.h>
+# include <SDL2/SDL_ttf.h>
 
 # include "Ship.h"
 # include "Laser.h"
